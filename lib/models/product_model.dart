80c03c828d632c86fe58c2e731aa5db539780d5c
mohamed <mezoattalla123@gmail.com>
2024-02-10 10:29:55 +0200

    create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,51 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductModel.fromJson(jsoData) {
+    return ProductModel(
+      id: jsoData['id'],
+      title: jsoData['title'],
+      price: jsoData['price'],
+      description: jsoData['description'],
+      category: jsoData['category'],
+      image: jsoData['image'],
+      rating: RatingModel.fromJson(
+        jsoData["rating"],
+      ),
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+
+  final int count;
+
+  RatingModel({
+    required this.rate,
+    required this.count,
+  });
+
+  factory RatingModel.fromJson(jsoData) {
+    return RatingModel(
+      count: jsoData["count"],
+      rate: jsoData["rate"],
+    );
+  }
+}
