efdaa226fea3b105dd8cf3fd4ceb76a827c24740
mohamed <mezoattalla123@gmail.com>
2024-02-13 04:09:05 +0200

    create grid view builder

--- lib/services/all_product_service.dart
+++ lib/services/all_product_service.dart
@@ -4,16 +4,16 @@ import 'package:store/models/product_model.dart';
 class AllProduceService {
   Future<List<ProductModel>> getAllProduct() async {
     List<dynamic> data =
-    await Api().get(url: 'https://fakestoreapi.com/products');
+        await Api().get(url: 'https://fakestoreapi.com/products');
+    print(data);
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(
+        ProductModel.fromJson(data[i]),
+      );
+    }
+    return productList;
 
-  
-      List<ProductModel> productModel = [];
-      for (int i = 0; i < data.length; i++) {
-        productModel.add(
-          ProductModel.fromJson(data[i]),
-        );
-      }
-      return productModel;
-    
+
   }
 }
